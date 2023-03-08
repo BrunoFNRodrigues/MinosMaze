e3b995aa91d0b31a3abe792192c77621e8ab9f63
BrunoFNRodrigues <bf17669@gmail.com>
2023-03-07 22:36:00 -0300

    Touro animado

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,12 +5,24 @@ using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
+    [SerializeField]
     public NavMeshAgent enemy;
-    public Transform player;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Transform transform;
 
+    public Transform player;
     // Update is called once per frame
     void Update()
     {
         enemy.SetDestination(player.position);
+        animator.SetBool("IsMoving", enemy.velocity.magnitude > 0.01f);
+
+        if(Vector3.Distance(player.position, transform.position) < 4.5f)
+        {
+            animator.SetTrigger("Attack");
+            
+        }
     }
 }
